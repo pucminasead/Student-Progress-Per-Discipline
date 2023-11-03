c5a45c6b5d2d48e5fdf7715021a1ec247d3df672
Túlio Magalhães Nogueira <tulio@pucminas.br>
2023-11-03 10:10:14 -0300

    Update to parallel execution

--- main.py
+++ main.py
@@ -1,13 +1,30 @@
 from src import start
 from src.database import get_courses_by_year
+import concurrent.futures
 
 
-course_years = [
-    2020,
-    2021,
-    2022,
-    2023
-]
-for year in course_years:
-    for course in get_courses_by_year(year):
-        start(course[3])
+# course_years = [
+#     2020,
+#     2021,
+#     2022,
+#     2023
+# ]
+# for year in course_years:
+#     for course in get_courses_by_year(year):
+#         start(course[3])
+
+courses = get_courses_by_year(1)
+
+with concurrent.futures.ThreadPoolExecutor(max_workers=12) as executor:
+    results = {
+        executor.submit(start, course[3]): course[3] for course in courses
+    }
+
+    for f in concurrent.futures.as_completed(results):
+        course = results[f]
+        try:
+            data = f.result()
+        except Exception as e:
+            print(f"{course} generated an exception: {e}")
+        else:
+            print(course, data) if data else print(course, "FINALIZADO")
